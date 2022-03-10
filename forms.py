33eca571def8bc80b212293c63f740ccfd522764
nacho <ignacio.contrerascabello@gmail.com>
2022-03-10 20:24:41 +0100

    Añadidos errores (hay que actualizar con una macro los mensajes que se muestran)

--- forms.py
+++ forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import validators
 
 class AddUserForm(FlaskForm):
-    name = StringField('Name: ')
-    email = StringField('Email: ')
-    topic = StringField('Topic: ')
+    name = StringField('Name: ',
+                       [validators.DataRequired(message='Por favor ingrese un nombre')])
+    email = StringField('Email: ',
+                        [validators.DataRequired(message='Por favor ingrese un email'),
+                         validators.Email(message='Ingrese un email valido')])
+    topic = StringField('Topic: ',
+                        [validators.DataRequired(message='Ingrese un tema')])
     submit = SubmitField('Register')
